57cb0efd430eea20a350cd94739129b2cddf6ec8
William Luangphinith <william.luangphinith@hublo.com>
2022-09-21 12:47:52 +0200

    project startup

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { abortOnError: false });
+  await app.listen(PORT);
+}
+bootstrap();
